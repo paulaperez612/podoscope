c84367256a4d58165f26ad8bf44a5a20b25bb3b6
Mauricio Neira <m.neira10@uniandes.edu.co>
2019-09-10 16:49:03 -0500

    cleaner code

---
+++ podoscope-ui/src/signIn/Copyright.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
+
+export default class Copyright extends Component {
+  render() {
+    return (
+
+      <Typography variant='body2' color='textSecondary' align='center'>
+        {'Copyright © '}
+        <Link color='inherit' href='https://material-ui.com/'>
+            Virtuai
+        </Link>{' '}
+        {new Date().getFullYear()}
+        {'.'}
+      </Typography>
+
+    );
+  }
+}
