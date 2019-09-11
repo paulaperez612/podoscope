dc7e9f94f470778d2db03af6a2a0cf1495f07c8e
Mauricio Neira <m.neira10@uniandes.edu.co>
2019-09-10 22:39:45 -0500

    image select template

--- podoscope-ui/src/mainView/ImageSelection.js
+++ podoscope-ui/src/mainView/ImageSelection.js
@@ -1,9 +1,64 @@
 import React, { Component } from 'react';
+import { Grid, Container, Typography, Paper } from '@material-ui/core';
 
 export default class ImageSelection extends Component {
-    render() {
-        return (
-            <h2>Hi</h2>
-        );
-    }
+  render() {
+    return (
+      <Grid container spacing={2}>
+
+        <Grid item xs={12}>
+          <Typography variant='h5' align='center'>
+            Image select
+          </Typography>
+        </Grid>
+
+
+        <Grid item xs={3}>
+          <Typography variant='h5'>
+            Back
+          </Typography>
+        </Grid>
+
+        <Grid item xs={3}>
+          <Paper>
+            Im1
+          </Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper>
+            Im2
+          </Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper>
+            Im3
+          </Paper>
+        </Grid>
+
+        <Grid item xs={3}>
+          <Typography variant='h5'>
+            Front
+          </Typography>
+        </Grid>
+
+        <Grid item xs={3}>
+          <Paper>
+            Im1
+          </Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper>
+            Im2
+          </Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper>
+            Im3
+          </Paper>
+        </Grid>
+
+      </Grid>
+
+    );
+  }
 }
