a1a08f1a279e29bb3c74f67efe18faa52653f488
Mauricio Neira <m.neira10@uniandes.edu.co>
2019-09-29 21:10:58 -0500

    front build

--- REST/app.js
+++ REST/app.js
@@ -23,7 +23,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'podoscope-ui/build')));
 
 app.use('/', indexRouter);
 app.use('/tryout', tryoutRouter);
